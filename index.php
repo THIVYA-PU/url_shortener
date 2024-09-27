11efc279066df97306aa79aa726df8b27d3220aa
Sample <you@example.com>
2024-09-27 23:31:51 +0530

    Logic for Shorten Url and adding the entries into DB

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
+<?php
+include 'config.php';
+if (isset($_POST['long_url'])){
+    $code = md5(uniqid(rand(1,5000),true));
+    $short_url = substr($code,0,6);
+    echo "$short_url";
+
+    $result = $mysqli->prepare("INSERT into urls (long_url,short_code) VALUES (?,?)");
+    $result->bind_param("ss",$_POST['long_url'],$short_url);
+    $result->execute();
+    $result->close();
+    
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,7 +21,7 @@
 </head>
 <body>
 <h1>URL Shortener</h1>
-    <form action="#" method="POST">
+    <form  method="POST">
         <label for="long_url">Enter URL:</label>
         <input type="url" id="long_url" name="long_url" required>
         <button type="submit">Shorten URL</button>
