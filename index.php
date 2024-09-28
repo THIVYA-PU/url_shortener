b243591eae3e7d76c74f4bdd1f934e6fc45161c6
Sample <you@example.com>
2024-09-28 07:32:28 +0530

    Logic for Redirection

--- index.php
+++ index.php
@@ -3,14 +3,21 @@ include 'config.php';
 if (isset($_POST['long_url'])){
     $code = md5(uniqid(rand(1,5000),true));
     $short_url = substr($code,0,6);
-    echo "$short_url";
-
     $result = $mysqli->prepare("INSERT into urls (long_url,short_code) VALUES (?,?)");
     $result->bind_param("ss",$_POST['long_url'],$short_url);
     $result->execute();
-    $result->close();
-    
+    $result->close(); 
 }
+
+if isset($_GET['short_code']){
+    $result = $mysqli->prepare("SELECT * FROM urls where short_code = ?");
+    $result->bind_param("s",$_GET['short_code']);
+    $result->execute();
+    $goto = $result->get_result()->ferch_array();
+    $g = $goto[1];
+    header("Location:$g");
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
